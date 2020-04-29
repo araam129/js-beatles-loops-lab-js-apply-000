7b2d3dbd2a151172d85d2246f364492829a7ad8b
Jennifer Han <araam129@gmail.com>
2020-04-29 23:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+function theBeatlesPlay (musiciansArray, instrumentsArray) {
+  let result = [];
+  
+  for (let i = 0; i < musiciansArray.length; i++) {
+    let string = String(musiciansArray[i]) + " plays " + String(instrumentsArray[i])
+    result.push(string)
+  }
+  return result;
+}
+
